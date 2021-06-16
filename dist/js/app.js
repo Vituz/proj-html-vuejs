a8966e4154047539e9c434b7fb896cd8d5e19a91
Vituz <vtbattaglia@gmail.com>
2021-06-17 00:20:20 +0200

    Complete Meet Section

--- dist/js/app.js
+++ dist/js/app.js
@@ -10,7 +10,23 @@
 var app = new Vue({
   el: '#app',
   data: {
-    menu: ['home', 'about', 'departments', 'articles', 'make appointment']
+    menu: ['home', 'about', 'departments', 'articles', 'make appointment'],
+    doctor: [{
+      name: 'Jon Snow',
+      role: 'Anesthesiologist',
+      img: 'doctor-1',
+      desc: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque'
+    }, {
+      name: 'Tony Stark',
+      role: 'Cardiologist',
+      img: 'doctor-2',
+      desc: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque'
+    }, {
+      name: 'Anna Smith',
+      role: 'Nurse Practitioner',
+      img: 'doctor-3',
+      desc: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque'
+    }]
   },
   methods: {}
 });
--- src/app.js
+++ src/app.js
@@ -11,6 +11,28 @@ const app = new Vue({
             'make appointment'
         ],
        
+        doctor:[
+            {
+                name: 'Jon Snow',
+                role: 'Anesthesiologist',
+                img: 'doctor-1',
+                desc: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque',
+            },
+
+            {
+                name: 'Tony Stark',
+                role: 'Cardiologist',
+                img: 'doctor-2',
+                desc: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque',
+            },
+
+            {
+                name: 'Anna Smith',
+                role: 'Nurse Practitioner',
+                img: 'doctor-3',
+                desc: 'At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque',
+            },
+        ]
     },
 
     methods:{
